b3380c5fe3f502a2e58007e4d2eac271a4642448
LaskiKa <kamil.laski0@gmail.com>
2023-03-05 10:39:28 +0100

    After first migration Added some relation correction in models

--- Loyalty_project/Loyalty_program_app/models.py
+++ Loyalty_project/Loyalty_program_app/models.py
@@ -13,11 +13,10 @@ class User(models.Model):
 
 class Invoices(models.Model):
     invoice_number = models.CharField(max_length=255)
-    buyer = models.ForeignKey(User, on_delete=models.CASCADE)
     vendor = models.CharField(max_length=255, blank=False)
     products = models.ManyToManyField("Products", through="InvoiceProductsList")
     sale_date = models.DateField(blank=False)
-    total_price = models.DecimalField(decimal_places=2)
+    total_price = models.DecimalField(decimal_places=2, max_digits=9)
 
 
 class InvoiceProductsList(models.Model):
@@ -28,5 +27,6 @@ class InvoiceProductsList(models.Model):
 
 class Products(models.Model):
     name = models.CharField(max_length=255)
-    unit_price = models.DecimalField(decimal_places=2)
+    unit_price = models.DecimalField(decimal_places=2, max_digits=9)
     basic_points = models.SmallIntegerField()
+    category = models.CharField(max_length=128)

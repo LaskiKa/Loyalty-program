8842fc93249d33340ffc364b2bfc79a4191fc9c6
LaskiKa <kamil.laski0@gmail.com>
2023-03-05 09:55:53 +0100

    Before first migration.
    Created models:
    -User
    -Invoices
    -InvoicesProductList
    -Products

--- Loyalty_project/Loyalty_program_app/models.py
+++ Loyalty_project/Loyalty_program_app/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
+
 # Create your models here.
+
+class User(models.Model):
+    name = models.CharField(max_length=128)
+    last_name = models.CharField(max_length=128)
+    password = models.CharField(max_length=64)
+    invoices = models.ForeignKey("Invoices", on_delete=models.CASCADE)
+    points = models.SmallIntegerField(default=0)
+
+
+class Invoices(models.Model):
+    invoice_number = models.CharField(max_length=255)
+    buyer = models.ForeignKey(User, on_delete=models.CASCADE)
+    vendor = models.CharField(max_length=255, blank=False)
+    products = models.ManyToManyField("Products", through="InvoiceProductsList")
+    sale_date = models.DateField(blank=False)
+    total_price = models.DecimalField(decimal_places=2)
+
+
+class InvoiceProductsList(models.Model):
+    invoice = models.ForeignKey(Invoices, on_delete=models.CASCADE)
+    products = models.ForeignKey("Products", on_delete=models.CASCADE)
+    qty = models.SmallIntegerField()
+
+
+class Products(models.Model):
+    name = models.CharField(max_length=255)
+    unit_price = models.DecimalField(decimal_places=2)
+    basic_points = models.SmallIntegerField()

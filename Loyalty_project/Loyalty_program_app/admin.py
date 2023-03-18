bf7529f369af3698b94b3c35bcf6f6f44479575a
LaskiKa <kamil.laski0@gmail.com>
2023-03-18 16:07:14 +0100

    Changes in models - trying add NIP

--- Loyalty_project/Loyalty_program_app/admin.py
+++ Loyalty_project/Loyalty_program_app/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from Loyalty_program_app.models import UserProfile, Products, Prizes
+
+admin.site.register(Products)
+admin.site.register(UserProfile)
+admin.site.register(Prizes)
 
 # Register your models here.
--- Loyalty_project/Loyalty_program_app/views.py
+++ Loyalty_project/Loyalty_program_app/views.py
@@ -21,6 +21,7 @@ class MainSite(View):
                       {'products': products})
 
 class SignUpView(CreateView):
+    """Sign up View"""
     form_class = UserCreationForm
     success_url = reverse_lazy("login")
     template_name = "registration/signup.html"
@@ -36,6 +37,7 @@ class UserMainSite(LoginRequiredMixin, View):
 
 
 class ProductAddView(LoginRequiredMixin, CreateView):
+    """"""
     model = Products
     fields = ["name", "unit_price", "basic_points", "category"]
     success_url = "/base/"  # do poprawki + zmiana tempaltki
